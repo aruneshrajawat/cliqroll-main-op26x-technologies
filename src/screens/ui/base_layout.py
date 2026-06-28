3efa646c752d1bd0a61b239cfade059fe27f4844
Arunesh Singh Rajawat <aruneshsinghrajawat@gmail.com>
2026-06-29 00:52:17 +0530

    new ui

--- src/screens/ui/base_layout.py
+++ src/screens/ui/base_layout.py
@@ -3,100 +3,133 @@ import streamlit as st
 
 def style_background_home():
     st.markdown("""
-    <style>
-        .stApp{
-            background:#5865F2 !important;
-        }
+        <style>
+                .stApp{
+                    background: #5865F2 !important;
+                }
 
-        .stApp div[data-testid="stColumn"]{
-            background:#BDB76B !important;
-            padding:2.5rem !important;
-            border-radius:5rem;
-        }
-    </style>
-    """, unsafe_allow_html=True)
-
-
+                .stApp div[data-testid ="stColumn"]{
+                    background-color:#BDB76B !important;
+                    padding:2.5rem !important;
+                    border-radius:5rem;
+                    }
+        </style>
+                """
+            , unsafe_allow_html=True)
+    
+    
 def style_background_dashboard():
     st.markdown("""
-    <style>
-        .stApp{
-            background:#FDFBD4 !important;
-        }
-    </style>
-    """, unsafe_allow_html=True)
+        <style>
+                .stApp{
+                    background: #FDFBD4 !important;
+                }
+        </style>
+                """
+            , unsafe_allow_html=True)
+        
 
+        
 
 def style_base_layout():
     st.markdown("""
-    <style>
+        <style>
+                /* Hide top tool bar of streamlit */
 
-    @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+                @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
+                @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&family=Outfit:wght@100..900&display=swap');
 
-    #MainMenu,
-    header,
-    footer{
-        visibility:hidden;
-    }
+                #MainMenu,footer,header{
+                    visibility:hidden;
+                }
 
-    [data-testid="stToolbar"],
-    [data-testid="stDecoration"],
-    [data-testid="stStatusWidget"]{
-        display:none !important;
-    }
+                /* Hide Streamlit toolbar and logo */
+                [data-testid="stToolbar"] {
+                    display: none !important;
+                }
 
-    .block-container{
-        padding-top:1.5rem !important;
-    }
+                [data-testid="stDecoration"] {
+                    display: none !important;
+                }
 
-    .stApp{
-        background:#BAC095 !important;
-    }
+                [data-testid="stStatusWidget"] {
+                    display: none !important;
+                }
 
-    h1{
-        font-family:'Climate Crisis',sans-serif !important;
-    }
+                .block-container{
+                    padding-top:1.5rem !important;
+                }
+                .stApp{
+                    background: #BAC095!important;
+                }
+                h1{
+                    font-family: 'Climate Crisis', sans-serif !important;
+                    font-size : 3.5rem !important;
+                    line-height: 1.1 !important;
+                    margin-bottom:0rem !important;
+                    color: #2C2C2C;
+                }
+                h2{
+                    font-family: 'Climate Crisis', sans-serif !important;
+                    font-size : 2rem !important;
+                    line-height: 1.1 !important;
+                    margin-bottom:0rem !important;
+                    color: #2C2C2C;
+                }
 
-    h2{
-        font-family:'Climate Crisis',sans-serif !important;
-    }
+                h3,h4,p{
+                    font-family: 'Outfit', sans-serif !important;
+                    color: #2C2C2C;
+                }
+                button[kind = "Secondary"]{
+                    border-radius: 1.5rem !important;
+                    background-color: #EB459E !important;
+                    color: white  !important;
+                    padding: 0.5rem 1.5rem !important;
+                    border: none !important;
+                    font-size: 1rem !important;
+                    transition: transform 0.3s ease in out !important;
+                    }
 
-    h3,h4,p,label,span{
-        font-family:'Outfit',sans-serif !important;
-    }
+                button[kind = "Tertiary"]{
+                    border-radius: 1.5rem !important;
+                    background-color: #5865F2 !important;
+                    color: white  !important;
+                    padding: 10px 20px !important;
+                    border: none !important;
+                    font-size: 1rem !important;
+                    transition: transform 0.3s ease in out !important;
+                    }
+                button{
+                    border-radius: 1.5rem !important;
+                    background-color: #FDFBD4 !important;
+                    color: black  !important;
+                    padding: 10px 20px !important;
+                    border: none !important;
+                    font-size: 1rem !important;
+                    transition: transform 0.3s ease in out !important;
+                    }
+                button:hover{
+                    transform: scale(1.05) !important;
+                }
+                div[role="dialog"],
+                div[data-testid="stDialog"],
+                div[data-testid*="Dialog"] {
+                    background-color: #FDFBD4 !important;
+                }
+                div[role="dialog"] *,
+                div[data-testid="stDialog"] *,
+                div[data-testid*="Dialog"]*  {
+                    color: #2C2C2C !important;
+                }
+                div[data-testid="stToast"] {
+                    background-color: #FDFBD4 !important;
+                }
+                div[data-testid="stToast"] * {
+                    color: #2C2C2C !important;
+                }
 
-    div[data-testid="stButton"] button{
-        background:#FDFBD4 !important;
-        color:#2C2C2C !important;
-        border:none !important;
-        border-radius:18px !important;
-    }
-
-    div[data-testid="stButton"] button:hover{
-        transform:scale(1.03);
-    }
-
-    div[role="dialog"]{
-        background:#FDFBD4 !important;
-    }
-
-    button[aria-label="Close"]{
-        background:transparent !important;
-        border:none !important;
-    }
-
-    button[aria-label="Close"] svg{
-        stroke:#2C2C2C !important;
-    }
-
-    [data-baseweb="toast"]{
-        background:#FDFBD4 !important;
-    }
-
-    [data-baseweb="toast"] *{
-        color:#2C2C2C !important;
-    }
-
-    </style>
-    """, unsafe_allow_html=True)
+                
+        </style>
+                """
+            , unsafe_allow_html=True)
